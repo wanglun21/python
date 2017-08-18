92ca938bc608616ba81e445ddaa892624e3459e8
wanglun21 <32929@sina.com>
2017-08-18 15:36:33 +0800

    xiu gai le hell.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 #coding= utf-8
 print ("Hello Word")
 print ("试试中文")
-print ("Python2.7想输入中文需要在文件开头加 #coding= utf-8\注意：coding后必须直接接=号，不能有空格，其他格式不限 ")       
+print ("Python2.7想输入中文需要在文件开头加 #coding= utf-8 \注意：coding后必须直接接'='号，不能有空格，其他格式不限 ")       
