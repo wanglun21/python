5326ecb14bbe3557805cc57cfd79e227094a148c
wanglun21 <32929@sina.com>
2017-08-18 15:47:48 +0800

    3

--- hello.py
+++ hello.py
@@ -2,4 +2,5 @@
 print ("Hello Word")
 print ("试试中文")
 print ("Python2.7想输入中文需要在文件开头加 #coding= utf-8 \注意：coding后必须直接接'='号，不能有空格，其他格式不限 ")
-print ("第二次修改")       
+print ("第二次修改")
+print ("第三次修改")       
